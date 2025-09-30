c08e25b0339447454b4a7a62b354deec3b0c5e06
baofc <baofcvcl@gmail.com>
2025-09-30 11:43:09 +0700

    add function chup anh

---
+++ DevExtremeAspNetCore/Models/NoteChiTietDonHang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevExtremeAspNetCore.Models;
+
+[Table("NoteChiTietDonHang")]
+public partial class NoteChiTietDonHang
+{
+    [Key]
+    [Column("IDNote")]
+    public int Idnote { get; set; }
+
+    [Column("IDCTDH")]
+    public int Idctdh { get; set; }
+
+    [StringLength(500)]
+    public string UrlFile { get; set; }
+
+    [StringLength(500)]
+    public string UrlImage { get; set; }
+
+    [StringLength(500)]
+    public string NoiDung { get; set; }
+}
